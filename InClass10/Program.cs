914aa4845a5e7b9198fe677e34f0fba373f9f891
Lydia Fritz <Lydia.Fritz@gcu.edu>
2018-04-03 12:04:46 -0700

    Comments removed

--- InClass10/Program.cs
+++ InClass10/Program.cs
@@ -17,15 +17,15 @@ namespace InClass10
             String textLine = sr.ReadToEnd( );
             int ct_lkf = 0;
 
-            //split into words
+            
             string[] allWords = textLine.Split( );
 
             foreach (string s in allWords)
             {
-                //make sure word is not empty
+               
                 if (s.Length > 0)
                 {
-                    //strip off ending non-alphas
+                   
                     string curr = s.ToLower( );
 
                     while (curr.Length > 0 && !Char.IsLetter( curr[ curr.Length - 1 ] ))
@@ -33,10 +33,9 @@ namespace InClass10
                         curr = curr.Substring( 0, curr.Length - 1 );
                     }
 
-                    //Console.WriteLine(curr);
+                    
                     if (curr.Length > 0)
-                    {
-                        //look at last letter
+                    {                        
                         if (curr[ curr.Length - 1 ] == 'e' ||
                             curr[ curr.Length - 1 ] == 't')
                             ct_lkf++;
